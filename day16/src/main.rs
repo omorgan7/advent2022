88f7822c185f39fb4f7572c8c752ca0d1f56a13b
Owen Morgan <omorgan7@me.com>
2022-12-25 12:26:38 +0000

    day 16

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,8 +1,7 @@
 use regex::Regex;
-use std::cell::RefCell;
 use std::cmp::Ordering;
 use std::collections::BinaryHeap;
-use std::rc::Rc;
+use std::collections::{BTreeMap, VecDeque};
 use std::vec::Vec;
 
 #[derive(Copy, Clone, Eq, PartialEq)]
@@ -26,95 +25,136 @@ impl PartialOrd for State {
     }
 }
 
-#[derive(Debug)]
-struct Valve {
-    label: String,
-    flow_rate: i64,
-    tunnels: Vec<Rc<RefCell<Valve>>>,
+fn part1_recursive(
+    current: usize,
+    visited: &Vec<(i64, i64)>,
+    flows: &[i64],
+    map: &[i64],
+    count: usize,
+    minutes_remaining: i64,
+) -> (i64, Vec<(i64, i64)>) {
+    if minutes_remaining <= 0 {
+        return (
+            visited
+                .iter()
+                .map(|(n, minute)| flows[*n as usize] * minute)
+                .sum(),
+            visited.clone(),
+        );
+    }
+
+    let neighbours = map[count * current..count * (current + 1)]
+        .iter()
+        .enumerate()
+        .filter(|(idx, m)| {
+            (minutes_remaining - *m - 1) * flows[*idx] > 0
+                && !visited.iter().any(|(node, _)| *node == (*idx as i64))
+        })
+        .map(|(idx, _)| idx);
+
+    let mut best = 0;
+    let mut best_visited = vec![];
+
+    for n in neighbours {
+        let mut new_visited = visited.clone();
+        new_visited.push((n as i64, minutes_remaining - map[count * current + n] - 1));
+
+        let (maybe_best, new_v) = part1_recursive(
+            n,
+            &new_visited,
+            flows,
+            map,
+            count,
+            minutes_remaining - map[count * current + n] - 1,
+        );
+
+        if maybe_best > best {
+            best = maybe_best;
+            best_visited = new_v.clone();
+        }
+    }
+
+    let v: &Vec<(i64, i64)> = if best_visited.is_empty() {
+        visited
+    } else {
+        &best_visited
+    };
+    (
+        v.iter()
+            .map(|(n, minute)| flows[*n as usize] * minute)
+            .sum(),
+        v.clone(),
+    )
 }
 
 fn part1(labels: &[String], flows: &[i64], map: &[i64], count: usize) -> i64 {
-    let mut minutes_remaining = 30;
+    let minutes_remaining = 30;
+    let opened = vec![];
 
-    let mut i = 0;
-
-    let mut opened = vec![];
-
-    while minutes_remaining > 0 {
-        let energy = map[i * count..(i + 1) * count]
+    part1_recursive(
+        labels
             .iter()
             .enumerate()
-            .map(|(idx, dist)| {
-                if opened.contains(&idx) {
-                    0
+            .find(|(_, l)| *l == "AA")
+            .unwrap()
+            .0,
+        &opened,
+        flows,
+        map,
+        count,
+        minutes_remaining,
+    )
+    .0
+}
+
+fn part2(labels: &[String], flows: &[i64], map: &[i64], count: usize) -> i64 {
+    let minutes_remaining = 26;
+
+    let start = labels
+        .iter()
+        .enumerate()
+        .find(|(_, l)| *l == "AA")
+        .unwrap()
+        .0;
+
+    let mut queue = VecDeque::new();
+
+    let mut best = BTreeMap::<Vec<usize>, i64>::new();
+    queue.push_back((minutes_remaining, start, 0, Vec::<usize>::new()));
+
+    while let Some((remaining, node, relief, visited)) = queue.pop_front() {
+        map[count * node..count * (node + 1)]
+            .iter()
+            .enumerate()
+            .filter(|(idx, m)| (remaining - *m - 1) * flows[*idx] > 0 && !visited.contains(idx))
+            .map(|(idx, _)| idx)
+            .for_each(|i| {
+                let mut new_visited = visited.clone();
+                new_visited.push(i);
+                let time = remaining - map[count * node + i] - 1;
+                queue.push_back((time, i, relief + time * flows[i], new_visited.clone()));
+                if best.contains_key(&new_visited) {
+                    let old = *best.get(&new_visited).unwrap();
+                    *best.get_mut(&new_visited).unwrap() =
+                        std::cmp::max(old, relief + time * flows[i]);
                 } else {
-                    flows[idx] * (minutes_remaining - dist - 1)
+                    best.insert(new_visited, relief + time * flows[i]);
                 }
-            })
-            .collect::<Vec<i64>>();
+            });
+    }
 
-        println!("{:?}", energy);
-        println!("--------------------");
-
-        let total_energy: i64 = energy.iter().sum();
-
-        let mut best = i64::MIN;
-        let mut best_idx = usize::MAX;
-
-        for (i0, e) in energy.iter().enumerate() {
-            if flows[i0] == 0 {
+    let mut best_flow = 0;
+    for (p0, f0) in &best {
+        for (p1, f1) in &best {
+            if p0.iter().any(|i| p1.contains(i)) {
                 continue;
             }
 
-            println!(
-                "{:?}",
-                map[i * count..(i + 1) * count]
-                    .iter()
-                    .enumerate()
-                    .map(|(i1, dist)| {
-                        if opened.contains(&i1) || i0 == i1 {
-                            0
-                        } else {
-                            flows[i1] * (minutes_remaining - map[i0 + i1 * count] - dist - 1)
-                        }
-                    })
-                    .collect::<Vec<i64>>()
-            );
-
-            let hypothetical_best_energy = map[i * count..(i + 1) * count]
-                .iter()
-                .enumerate()
-                .map(|(i1, dist)| {
-                    if opened.contains(&i1) || i1 == i0 {
-                        0
-                    } else {
-                        flows[i1] * (minutes_remaining - map[i0 + i1 * count] - dist - 1)
-                    }
-                })
-                .sum::<i64>();
-
-            if hypothetical_best_energy > best {
-                best_idx = i0;
-                best = hypothetical_best_energy;
-            }
+            best_flow = std::cmp::max(f0 + f1, best_flow);
         }
-
-        println!("Opening: {}", best_idx);
-        if best_idx == usize::MAX {
-            break;
-        }
-
-        opened.push(best_idx);
-        minutes_remaining -= map[i * count + best_idx];
-        i = best_idx;
     }
 
-    print!("Opened: ");
-    for o in opened {
-        print!("{} ", o);
-    }
-    println!();
-    0
+    best_flow
 }
 
 fn dijkstra(map: &mut [i64], mapcount: usize) {
@@ -169,17 +209,6 @@ fn dijkstra(map: &mut [i64], mapcount: usize) {
             map[end * mapcount + start] = dist[end];
         }
     }
-
-    for y in 0..mapcount {
-        for x in 0..mapcount {
-            if map[x + mapcount * y] < 10 {
-                print!(" ");
-            }
-            print!("{} ", map[x + mapcount * y]);
-        }
-        println!();
-    }
-    println!();
 }
 
 fn main() {
@@ -188,7 +217,7 @@ fn main() {
     let re = Regex::new(r"Valve ([A-Z]{2}) has flow rate=(\d+); tunnels? leads? to valves? (.+)")
         .unwrap();
 
-    let mut valve_count = input.lines().count();
+    let valve_count = input.lines().count();
 
     let mut labels = vec![];
     let mut flows = vec![];
@@ -212,7 +241,6 @@ fn main() {
 
         this_tunnels.for_each(|tunnel| {
             if let Some(edge) = labels.iter().position(|label| *label == tunnel) {
-                println!("Found {} for {} at index: {}", tunnel, labels[idx], edge);
                 tunnels[valve_count * edge + idx] = 1;
                 tunnels[valve_count * idx + edge] = 1;
             }
@@ -222,33 +250,5 @@ fn main() {
     dijkstra(&mut tunnels, valve_count);
 
     println!("{}", part1(&labels, &flows, &tunnels, valve_count));
-
-    // print!("  ");
-    // for label in &labels {
-    //     print!("{} ", label.chars().next().unwrap());
-    // }
-    // println!();
-    // for y in 0..valve_count {
-    //     print!("{} ", &labels[y].chars().next().unwrap());
-    //     for x in 0..valve_count {
-    //         print!("{} ", tunnels[x + valve_count * y]);
-    //     }
-    //     println!();
-    // }
-    // println!("{:?}", tunnels);
-
-    // for v in &valves {
-    //     let vv = v.borrow();
-    //     println!(
-    //         "Valve: {{ label: {}, flow_rate: {}, tunnels: {:?} }}",
-    //         vv.label,
-    //         vv.flow_rate,
-    //         vv.tunnels
-    //             .iter()
-    //             .map(|t| t.borrow().label.to_string())
-    //             .collect::<Vec<String>>()
-    //     );
-    // }
-
-    // println!("{}", part1(Rc::clone(&valves[0]), None, vec![], 0, 0));
+    println!("{}", part2(&labels, &flows, &tunnels, valve_count));
 }
